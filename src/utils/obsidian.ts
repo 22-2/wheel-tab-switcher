2c4155ef27cf9f3f5bc210e574931f4fb485244b
22-2 <17890m@protonmail.ch>
2025-03-10 20:06:17 +0900

    chore: add comments to getWorkspaceItems

--- src/utils/obsidian.ts
+++ src/utils/obsidian.ts
@@ -6,6 +6,14 @@ import {
 	App,
 } from "obsidian";
 
+/**
+ * Retrieves workspace items of a specific type from all workspace leaves.
+ *
+ * @param app The application instance.
+ * @param getItem A function that takes a workspace leaf and returns the workspace item or null/undefined if not found.
+ * @returns An array of workspace items of type T.
+ * @template T - The type of workspace item, must be either WorkspaceWindow or WorkspaceParent.
+ */
 function getWorkspaceItems<T extends WorkspaceWindow | WorkspaceParent>(
 	app: App,
 	getItem: (leaf: WorkspaceLeaf) => T | null | undefined,
@@ -13,7 +21,7 @@ function getWorkspaceItems<T extends WorkspaceWindow | WorkspaceParent>(
 	const itemMap = new Map<string, T>();
 	getAllLeaves(app).forEach((leaf) => {
 		const item = getItem(leaf);
-		if (item && item.id) { // item が nullish でないことを確認し、id プロパティの存在をチェック
+		if (item && item.id) {
 			itemMap.set(item.id, item);
 		}
 	});
