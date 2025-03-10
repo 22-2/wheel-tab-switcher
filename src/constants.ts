f16eac50569adf8e82e16f4084eefea2291951a7
22-2 <17890m@protonmail.ch>
2025-03-11 00:19:36 +0900

    refactor: remove dead code

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,1 @@
 export const PLUGIN_ID = "wheel-tab-switcher";
-
-export enum LogLevel {
-	Silly = 0,
-	Trace = 1,
-	Debug = 2,
-	Info = 3,
-	Warn = 4,
-	Error = 5,
-	Fatal = 6,
-	Off = 7, // ログ出力を完全に無効にするレベル
-}
