59be3c10c1da8cd0a0e26d539625ed79b1e5e9b2
22-2 <17890m@protonmail.ch>
2025-03-19 23:30:52 +0900

    chore: Add top bar scroll wheel tab switching setting

--- src/settings.ts
+++ src/settings.ts
@@ -8,6 +8,21 @@ export class WheelTabSwitcherSettingTab extends PluginSettingTab {
 
 	display(): void {
 		this.containerEl.empty();
+
+		new Setting(this.containerEl).setName("⚙️ Wheel Tab Switcher Settings").setHeading();
+
+		new Setting(this.containerEl)
+			.setName("Top Bar Scroll Wheel Tab Switching")
+			.setDesc("Allows you to switch tabs by scrolling the mouse wheel over the top bar (non-tab area) of the file pane.") // setDesc を追加
+			.addToggle((toggle) => {
+				toggle.setValue(this.plugin.settings.topBarScrollWheelTabSwitch).onChange(async (val) => {
+					this.plugin.settings.topBarScrollWheelTabSwitch = val;
+					await this.plugin.saveSettings();
+				});
+			});
+
+		new Setting(this.containerEl).setName("🐞 Dev").setHeading();
+
 		new Setting(this.containerEl).setName("debug").addToggle((toggle) => {
 			toggle.setValue(this.plugin.settings.debug).onChange(async (val) => {
 				this.plugin.settings.debug = val;
@@ -19,8 +34,10 @@ export class WheelTabSwitcherSettingTab extends PluginSettingTab {
 
 export interface Settings {
 	debug: boolean;
+	topBarScrollWheelTabSwitch: boolean;
 }
 
 export const DEFAULT_SETTINGS: Settings = {
 	debug: false,
+	topBarScrollWheelTabSwitch: false,
 };
